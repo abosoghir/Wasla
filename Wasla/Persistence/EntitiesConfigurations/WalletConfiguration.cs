0d7f4cdf32243fbded9e1232e37b5ba3bcf4fd68
abosoghir <abosoghir2022@gmail.com>
2026-04-19 18:40:38 +0200

    add WalletConfiguration

---
+++ Wasla/Persistence/EntitiesConfigurations/WalletConfiguration.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Wasla.Persistence.EntitiesConfigurations;
+
+public class WalletConfiguration : IEntityTypeConfiguration<Wallet>
+{
+    public void Configure(EntityTypeBuilder<Wallet> builder)
+    {
+
+        builder.HasKey(w => w.Id);
+
+        builder.Property(w => w.UserId)
+            .HasMaxLength(100)
+            .IsRequired();
+
+        builder.Property(w => w.Currency)
+            .HasConversion<int>()
+            .IsRequired()
+            .HasDefaultValue(CurrencyType.EGP);
+
+        builder.Property(w => w.Balance)
+            .HasPrecision(18, 2)
+            .IsRequired()
+            .HasDefaultValue(0);
+
+        builder.Property(w => w.TotalDeposited)
+            .HasPrecision(18, 2)
+            .IsRequired()
+            .HasDefaultValue(0);
+
+        builder.Property(w => w.TotalWithdrawn)
+            .HasPrecision(18, 2)
+            .IsRequired()
+            .HasDefaultValue(0);
+
+        builder.Property(w => w.IsActive)
+            .IsRequired()
+            .HasDefaultValue(true);
+
+        builder.HasOne(w => w.User)
+            .WithMany(u => u.Wallets)
+            .HasForeignKey(w => w.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasMany(w => w.Transactions)
+            .WithOne(t => t.Wallet)
+            .HasForeignKey(t => t.WalletId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
