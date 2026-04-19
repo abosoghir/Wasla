23ecda5f3a2116deda869c84a6791d4d1f577415
abosoghir <abosoghir2022@gmail.com>
2026-04-19 16:17:07 +0200

    Add Project Configuration

---
+++ Wasla/Persistence/EntitiesConfigurations/ProjectConfiguration.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Wasla.Persistence.EntitiesConfigurations;
+
+public class ProjectConfiguration : IEntityTypeConfiguration<Project>
+{
+    public void Configure(EntityTypeBuilder<Project> builder)
+    {
+
+        builder.HasKey(p => p.Id);
+
+
+        builder.Property(p => p.Title)
+            .HasMaxLength(200)
+            .IsRequired();
+
+        builder.Property(p => p.Description)
+            .HasMaxLength(1000)
+            .IsRequired();
+
+        builder.Property(p => p.RequiredSkills)
+            .HasMaxLength(1000);
+
+        builder.Property(p => p.DurationDays)
+            .IsRequired();
+
+        builder.Property(p => p.IsPublic)
+            .HasDefaultValue(true);
+
+        builder.Property(p => p.Status)
+            .HasConversion<int>()
+            .IsRequired();
+
+        builder.Property(p => p.Category)
+            .HasConversion<int>()
+            .IsRequired();
+
+        builder.Property(p => p.Budget)
+            .HasPrecision(18, 2)
+            .IsRequired();
+
+
+        builder.HasOne(p => p.Seeker)
+            .WithMany(s => s.Projects)
+            .HasForeignKey(p => p.SeekerId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasMany(p => p.Milestones)
+            .WithOne(m => m.Project)
+            .HasForeignKey(m => m.ProjectId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasMany(p => p.Offers)
+            .WithOne(o => o.Project)
+            .HasForeignKey(o => o.ProjectId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasMany(p => p.Attachments)
+            .WithOne(a => a.Project)
+            .HasForeignKey(a => a.ProjectId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasIndex(p => p.SeekerId);
+        builder.HasIndex(p => p.Status);
+        builder.HasIndex(p => p.Category);
+    }
+}
+
