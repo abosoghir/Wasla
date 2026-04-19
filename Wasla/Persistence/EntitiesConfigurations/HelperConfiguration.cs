f3acfca1f4fcd0696433c3eed1a8b97964b498b8
Hisham-sayed <hishamsayed995@gmail.com>
2026-04-19 17:13:38 +0200

    config

--- Wasla/Persistence/EntitiesConfigurations/HelperConfiguration.cs
+++ Wasla/Persistence/EntitiesConfigurations/HelperConfiguration.cs
@@ -9,20 +9,61 @@ public class HelperConfiguration : IEntityTypeConfiguration<Helper>
     {
         builder.HasKey(h => h.Id);
 
-        builder.Property(h => h.UserId).HasMaxLength(100);
-        builder.Property(h => h.Headline).HasMaxLength(200);
-        builder.Property(h => h.Location).HasMaxLength(200);
-        builder.Property(h => h.HourlyRate).HasPrecision(18, 2);
-        builder.Property(h => h.TotalEarnings).HasPrecision(18, 2);
+        builder.Property(h => h.UserId)
+            .HasMaxLength(100)
+            .IsRequired();
+
+        builder.Property(h => h.Headline)
+            .HasMaxLength(200);
+
+        builder.Property(h => h.Location)
+            .HasMaxLength(200);
+
+        builder.Property(h => h.HourlyRate)
+            .HasPrecision(18, 2)
+            .IsRequired();
+
+        builder.Property(h => h.TotalEarnings)
+            .HasPrecision(18, 2)
+            .IsRequired();
+
+        builder.Property(h => h.IsAvailable)
+            .IsRequired()
+            .HasDefaultValue(true);
+
+        builder.Property(h => h.IsVerified)
+            .IsRequired()
+            .HasDefaultValue(false);
+
+        builder.Property(h => h.Points)
+            .IsRequired();
+
+        builder.Property(h => h.LifetimePoints)
+            .IsRequired();
+
+        builder.Property(h => h.CompletedTasksCount)
+            .IsRequired();
+
+        builder.Property(h => h.CompletedSessionsCount)
+            .IsRequired();
+
+        builder.Property(h => h.CompletedProjectsCount)
+            .IsRequired();
+
+        builder.Property(h => h.SpeedOfResponseInMintues)
+            .IsRequired();
+
+        builder.Property(h => h.AverageRating)
+            .IsRequired();
+
+        builder.Property(h => h.TotalReviewsCount)
+            .IsRequired();
+
+        builder.Ignore(h => h.IsNextTaskFree);
 
         builder.HasOne(h => h.User)
             .WithOne(u => u.Helper)
             .HasForeignKey<Helper>(h => h.UserId)
             .OnDelete(DeleteBehavior.Cascade);
-
-        builder.HasIndex(h => h.UserId).IsUnique();
-        builder.HasIndex(h => h.IsAvailable);
-        builder.HasIndex(h => h.IsVerified);
-        builder.HasIndex(h => h.AverageRating);
     }
 }
